b73dab3dc73d67849711c210ad0ddc0e39912782
TeslaAngel <mohadot@outlook.com>
2025-05-20 21:18:41 -0700

    TODO 1 to 3

--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -21,7 +21,7 @@ describe('Basic user flow for Website', () => {
   // Check to make sure that all 20 <product-item> elements have data in them
   // We use .skip() here because this test has a TODO that has not been completed yet.
   // Make sure to remove the .skip after you finish the TODO. 
-  it.skip('Make sure <product-item> elements are populated', async () => {
+  it('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -38,10 +38,18 @@ describe('Basic user flow for Website', () => {
     console.log(`Checking product item 1/${prodItemsData.length}`);
 
     // Make sure the title, price, and image are populated in the JSON
-    firstValue = prodItemsData[0];
-    if (firstValue.title.length == 0) { allArePopulated = false; }
+    //firstValue = prodItemsData[0];
+    /*if (firstValue.title.length == 0) { allArePopulated = false; }
     if (firstValue.price.length == 0) { allArePopulated = false; }
-    if (firstValue.image.length == 0) { allArePopulated = false; }
+    if (firstValue.image.length == 0) { allArePopulated = false; }*/
+
+    for (let i = 0; i < prodItemsData.length; i++) {
+      const data = prodItemsData[i];
+      if (!data.title || !data.price || !data.image) {
+        allArePopulated = false;
+        break;
+      }
+    }
 
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
@@ -57,8 +65,15 @@ describe('Basic user flow for Website', () => {
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
   // the button swaps to "Remove from Cart"
-  it.skip('Clicking the "Add to Cart" button should change button text', async () => {
+  it('Clicking the "Add to Cart" button should change button text', async () => {
     console.log('Checking the "Add to Cart" button...');
+
+    const prodItem = await page.$('product-item');
+    const shadowRoot = await prodItem.getProperty('shadowRoot');
+    const buttonHandle = await shadowRoot.$('button');
+    await buttonHandle.click();
+    const buttonText = await (await buttonHandle.getProperty('innerText')).jsonValue();
+    expect(buttonText).toBe('Remove from Cart');
 
     /**
      **** TODO - STEP 2 **** 
@@ -73,8 +88,17 @@ describe('Basic user flow for Website', () => {
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
   // number in the top right has been correctly updated
-  it.skip('Checking number of items in cart on screen', async () => {
+  it('Checking number of items in cart on screen', async () => {
     console.log('Checking number of items in cart on screen...');
+
+    const prodItems = await page.$$('product-item');
+    for (let item of prodItems) {
+      const shadowRoot = await item.getProperty('shadowRoot');
+      const button = await shadowRoot.$('button');
+      await button.click();
+    }
+    const cartCount = await page.$eval('#cart-count', el => el.innerText);
+    expect(cartCount).toBe('20');
 
     /**
      **** TODO - STEP 3 **** 
